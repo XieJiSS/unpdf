a3e33356371a9f94aa859f2ec0998bb043434e84
jiejiss <c141028@protonmail.com>
2017-11-26 22:27:54 +0800

    update: 更新的流程 finished

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
-echo 请在ConEmu中运行，并及时根据版本修改install.nsi
+echo 请在ConEmu中运行，并及时根据版本修改本脚本、install.nsi和package.json
 pause
-cmd /c electron-packager . un_pdf_downloader --app-version="1.3.2" --ignore="(build.cmd|pack.cmd|clean.cmd|install.nsi|license.txt|make.bat)" --app-copyright="(c) JieJiSS 2017" --platform=win32 --arch=ia32 --out=".\out" --overwrite --icon=".\icon.ico" --download.mirror="https://npm.taobao.org/mirrors/electron/" --package-manager=npm --electron-version="1.7.9"
+cmd /c electron-packager . un_pdf_downloader --app-version="1.4.1" --ignore="(build.cmd|pack.cmd|clean.cmd|install.nsi|license.txt|make.bat)" --app-copyright="(c) JieJiSS 2017" --platform=win32 --arch=ia32 --out=".\out" --overwrite --icon=".\icon.ico" --download.mirror="https://npm.taobao.org/mirrors/electron/" --package-manager=npm --electron-version="1.7.9"
